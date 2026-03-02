5912ec57bcf844abe4955d74a0972734871746f7
Rodrigo <2148722@student.uma.pt>
2026-03-02 23:58:31 +0000

    mainft3

--- fichaTrabalho3/ArrayUtils.js
+++ fichaTrabalho3/ArrayUtils.js
@@ -264,8 +264,8 @@
 // console.log(contains(array, 3));
 
 module.exports = {
-    isEmpty : (array) =>{
-        return array.length===0;
+    isEmpty: (array) => {    
+        return array.length === 0;        
     },
 
     max: (array) => {
@@ -285,17 +285,73 @@ module.exports = {
         }
         return min;
     },
-
-    average:(array) => {
+    
+    average: (array) => {
         let sum = 0;
         for (let i = 0; i < array.length; i++) {
             sum += array[i];
-            
         }
         return sum / array.length;
     },
 
-    indeOf:(array, value) => {
-        
+    indexOf: (array, value) => {
+        for (let i = 0; i < array.length; i++) {
+            if(array[i] === value)        
+                return i;
+        }
+        return -1;
+    },
+
+    subArray: (array, start, end) => {
+        let subArray = [];
+        for (let i = start; i <= end; i++) {
+            subArray.push(array[i]);
+        }
+        return subArray;
+    },
+
+    isSameLength: (arr1, arr2) => {
+        return arr1.length === arr2.length;
+    },
+
+    reverse: (array) => {
+        let arrayRev = [];
+        for (let i = array.length-1; i >= 0; i--) {
+            arrayRev.push(array[i]); 
+        }
+        return arrayRev;
+    },
+    
+    swap: (array, index1, index2) => {
+        let array2 = [];
+        for (let i = 0; i < array.length; i++) {
+            array2.push(array[i]);
+        }
+        // alterar arry original
+        let int = array2[index1];
+        array2[index1] = array2[index2];
+        array2[index2] = int;
+
+        return array2;
+    },
+    
+    contains: (array, value) => {
+        for (let i = 0; i < array.length; i++) {
+            if(array[i] === value)        
+                return true
+        }
+        return false;
+    },
+    
+    concatenate: (a1, a2) => {
+        let array = [];
+        for (let i = 0; i < a1.length; i++) {
+            array.push(a1[i]);
+        }
+
+        for (let i = 0; i < a2.length; i++) {
+            array.push(a2[i]);        
+        }
+        return array
     }
-}
+};
--- fichaTrabalho3/testes.js
+++ fichaTrabalho3/testes.js
@@ -1,3 +1,14 @@
-const arrayUtils = require('./arrayUtils');
+const arrayUtils = require("./ArrayUtils");
 
+const array1 = [1, 2, 3, 5, 4, 7, 8, 9, 6];
+const array2 = [2, 5, 4, 7, 3, 6, 5];
+const array3 = [4, 7, 9, 8, 5];
+const array4 = [5, 4, 7, 9, 3, 6, 5];
 
+console.log(arrayUtils.max(array1));
+console.log(arrayUtils.average(array1));
+console.log(arrayUtils.min(array2));
+console.log(arrayUtils.reverse(array3));
+console.log(arrayUtils.concatenate(array4, array2));
+console.log(arrayUtils.swap(array1, 6, 3));
+console.log(arrayUtils.contains(array1, 6));
