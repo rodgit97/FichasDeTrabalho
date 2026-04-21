6aecf39ec6113816547807f8f1cdd306e02217f2
Rodrigo <2148722@student.uma.pt>
2026-04-21 18:24:40 +0100

    main8_1

---
+++ fichaTrabalho8/readme.md
@@ -0,0 +1,5 @@
+<!-- inicializar pasta -->
+npm init -y
+<!-- instalar dependencias -->
+npm install express mysql2
+<!-- criar ficheiro app.js -->
---
+++ fichaTrabalho8/swagger.js
@@ -0,0 +1,6 @@
+const swaggerAutogen = require("swagger-autogen")(); // importa a biblioteca swagger-autogen para gerar a documentacao Swagger automaticamente.
+const outputFile = "./swagger_output.json"; //para criar o interface do swagger
+const endpointsFiles = ["./app.js"]; //para ler os endpoints do app.js
+
+swaggerAutogen(outputFile, endpointsFiles, doc); // gera a documentacao Swagger automaticamente, lendo os endpoints definidos em app.js e salvando a documentacao no arquivo swagger_output.json.
+
